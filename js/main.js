00a4be22450a7241540f74ddab8a57612db63531
ShinJaeIl01 <ekvm555@naver.com>
2022-04-24 00:58:49 +0900

    feat: Creat Badge

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // 오늘 날짜
+// 오늘 날짜
 const date = new Date()
 const week = new Array('일', '월', '화', '수', '목', '금', '토')
 
@@ -7,3 +8,16 @@ day = date.getDate()
 dd = date.getDay()
 document.getElementById('current_date').innerHTML = (("00"+month.toString()).slice(-2)) + '월' + ' ' + (("00"+day.toString()).slice(-2)) + '일' + ' ' + week[date.getDay()] + '요일'
 // document.getElementById('current_date').innerHTML = '0' + month + '월' + ' ' + day + '일' + ' ' + week[date.getDay()] + '요일'
+
+
+// 배지 클릭 이벤트  
+// 배지 클릭 이벤트
+const badgesEl = document.querySelector('.badges')
+const badgeRed = document.querySelector('.red')
+const store = document.querySelector('.store')
+
+function toggleQuickSubMenu() {
+  store.classList.add('block')
+}
+
+badgeRed.addEventListener("click",toggleQuickSubMenu)
