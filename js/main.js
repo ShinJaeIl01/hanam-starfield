17e44c25b48dea271b96e2f82bd29afeddb784f2
ShinJaeIl01 <ekvm555@naver.com>
2022-04-24 21:47:24 +0900

    feat: Create Main Title and Event

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ document.getElementById('current_date').innerHTML = (("00"+month.toString()).sli
 // document.getElementById('current_date').innerHTML = '0' + month + '월' + ' ' + day + '일' + ' ' + week[date.getDay()] + '요일'
 
 
+
 // 배지 클릭 이벤트  
 // 배지 클릭 이벤트
 const badgesEl = document.querySelector('.badges')
@@ -23,6 +24,8 @@ function toggleQuickSubMenu() {
 badgeRed.addEventListener("click",toggleQuickSubMenu)
 
 
+
+// 상단 이동
 // 상단 이동
 const topEl = document.querySelector('.topBtn');
 
@@ -31,3 +34,51 @@ topEl.addEventListener('click', function () {
     scrollTo: 0
   })
 })
+
+
+
+// 메인이미지 스와이퍼
+// 메인이미지 스와이퍼
+
+// new Swiper('.topMain .swiper', {
+//   slidesPerView: 1, //한번에 보여줄 슬라이드 개수
+//   loop: true,
+//   // autoplay: {
+//   //   delay: 5000
+//   // },
+//   pagination: {
+//     el: '.topMain .swiper-pagination', //페이지 번호 요소 선택자
+//     clickable: true //사용자의 페이지 번호 요소 제어
+//   },
+//   navigation: {
+//     prevEl: '.topMain .swiper-prev',
+//     nextEl: '.topMain .swiper-next'
+//   }
+// });
+
+
+
+new Swiper('.topMain .swiper', {
+  loop: true,
+  autoplay: {
+    delay: 4000
+  },
+  pagination: {
+    el: '.topMain .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true //사용자의 페이지 번호 요소 제어
+  },
+  
+  navigation: {
+    prevEl: '.topMain .swiper-prev',
+    nextEl: '.topMain .swiper-next',
+  }
+});
+
+// $('.start').on('click',function(){
+//   swiper.autoplay.start();
+//   return false;
+//  })
+// $('.stop').on('click',function(){
+//   swiper.autoplay.stop();
+//   return false;
+//  });
